673e75846c17151e9e094d8fbf692d02dcba023a
gedesurya125 <gedesurya125@gmail.com>
2023-06-30 12:54:38 +0800

    remove unrelated package

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import { performRequest } from 'lib/datocms';
 import { draftMode } from 'next/headers';
 import { LandingPage } from 'section';
-import { DraftLandingPage } from 'section/landingPage/DraftLandindingPage';
 
 const LANDING_PAGE_QUERY = `
   query LandingPage {
@@ -15,8 +14,6 @@ const LANDING_PAGE_QUERY = `
 `;
 
 export default async function Home() {
-  // const { isEnabled } = draftMode();
-
   function getPageRequest() {
     const { isEnabled } = draftMode();
     return { query: LANDING_PAGE_QUERY, includeDrafts: isEnabled };
@@ -25,19 +22,6 @@ export default async function Home() {
   const pageRequest = getPageRequest();
 
   const data = await performRequest(pageRequest);
-
-  // if (isEnabled) {
-  //   return (
-  //     <DraftLandingPage
-  //       subscription={{
-  //         ...pageRequest,
-  //         initialData: data,
-  //         token: process.env.NEXT_DATOCMS_API_TOKEN,
-  //         environment: process.env.NEXT_DATOCMS_ENVIRONMENT || null
-  //       }}
-  //     />
-  //   );
-  // }
 
   return <LandingPage data={data} />;
 }
